e71a05120b3a55d9156f4249a1333d598dfdaed9
Bulat Karimov <bulatkarimov96@gmail.com>
2023-04-09 21:11:53 +0400

    add ip resource create

---
+++ app/actions/ips/create.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module IpsService
+  module Actions
+    module Ips
+      class Create < IpsService::Action
+        include Deps["repos.ip_repo"]
+
+        params Params::Create
+
+        def handle(request, response)
+          halt 422, {errors: request.params.errors}.to_json unless request.params.valid?
+
+          # TODO PG::UniqueViolation rescue
+          ip = ip_repo.create(ip_address: request.params[:ip], enabled: request.params[:enabled])
+
+          response.status = 201
+          response.body = ip.to_json
+        end
+      end
+    end
+  end
+end
---
+++ app/actions/ips/params/create.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'byebug'
+require "ipaddress"
+
+module IpsService
+  module Actions
+    module Ips
+      module Params
+        class Create < Hanami::Action::Params
+
+          #TODO ipv4/ipv6 format check
+          params do
+            optional(:enabled).value(:bool)
+            required(:ip).filled(:string)
+          end
+        end
+      end
+    end
+  end
+end
