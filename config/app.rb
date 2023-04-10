604ccbb78b233cf8d090cd03adbcf2638b09b9dc
Bulat Karimov <bulatkarimov96@gmail.com>
2023-04-10 22:42:54 +0400

    add hanami logger

--- config/app.rb
+++ config/app.rb
@@ -5,5 +5,15 @@ require 'hanami'
 module IpsService
   class App < Hanami::App
     config.middleware.use :body_parser, :json
+
+    environment(:development) do
+      config.logger.stream = $stdout
+
+      config.logger.options[:colorize] = true
+
+      config.logger.template = <<~TMPL
+        [<blue>%<progname>s</blue>] [%<severity>s] [<green>%<time>s</green>] %<message>s %<payload>s
+      TMPL
+    end
   end
 end
