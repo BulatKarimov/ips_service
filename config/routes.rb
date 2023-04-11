9653d55b32a723fe7f43e566fb4e2d16d9e07dd3
Bulat Karimov <bulatkarimov96@gmail.com>
2023-04-11 15:03:10 +0400

    wrap api into api/v1 namespace

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,16 @@ require 'sidekiq/cron/web'
 module IpsService
   class Routes < Hanami::Routes
     root { 'Hello from Hanami' }
-
-    post '/ips', to: 'ips.create'
-    post '/ips/:id/enable', to: 'ips.enable'
-    post '/ips/:id/disable', to: 'ips.disable'
-    delete '/ips/:id', to: 'ips.destroy'
-    delete '/ips/:id', to: 'ips.destroy'
-    get '/ip_stats', to: 'ip_stats.index'
+    scope :api do
+      scope :v1 do
+        post '/ips', to: 'ips.create'
+        post '/ips/:id/enable', to: 'ips.enable'
+        post '/ips/:id/disable', to: 'ips.disable'
+        delete '/ips/:id', to: 'ips.destroy'
+        delete '/ips/:id', to: 'ips.destroy'
+        get '/ips/:id/ip_stats', to: 'ip_stats.index'
+      end
+    end
 
     mount Sidekiq::Web, at: '/sidekiq'
   end
