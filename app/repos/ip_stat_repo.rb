91f1d0894eaeac86df458f4df2ebed1845c92434
Bulat Karimov <bulatkarimov96@gmail.com>
2023-04-13 21:02:09 +0400

    tmp

--- app/repos/ip_stat_repo.rb
+++ app/repos/ip_stat_repo.rb
@@ -1,27 +1,40 @@
 # frozen_string_literal: true
 
-require 'securerandom'
-
 module IpsService
   module Repos
     class IpStatRepo
       include Deps['clickhouse.connection']
 
       def get_stats_info(start_time:, end_time:, ip_address:)
-        statement = connection.prepare("
-          SELECT
-            ip_address,
-            AVG(if(rtt = 0, NULL, rtt)) AS avg_rtt,
-            MIN(if(rtt = 0, NULL, rtt)) AS min_rtt,
-            MAX(if(rtt = 0, NULL, rtt)) AS max_rtt,
-            quantile(0.5)(if(rtt > 0, rtt, NULL)) AS median_rtt,
-            sqrt(VAR_POP(if(rtt > 0, rtt, NULL))) AS std_dev_rtt,
-            (countIf(rtt = 0) / count(*)) * 100 AS packet_loss_percent
-          FROM ip_stats
-          WHERE timestamp >= ? AND timestamp < ? AND ip_address = ?
-        ")
+        # statement = connection.prepare_statement("
+        #   SELECT
+        #     ip_address,
+        #     AVG(if(rtt = 0, NULL, rtt)) AS avg_rtt,
+        #     MIN(if(rtt = 0, NULL, rtt)) AS min_rtt,
+        #     MAX(if(rtt = 0, NULL, rtt)) AS max_rtt,
+        #     quantile(0.5)(if(rtt > 0, rtt, NULL)) AS median_rtt,
+        #     sqrt(VAR_POP(if(rtt > 0, rtt, NULL))) AS std_dev_rtt,
+        #     (countIf(rtt = 0) / count(*)) * 100 AS packet_loss_percent
+        #   FROM ip_stats
+        #   WHERE timestamp >= ? AND timestamp < ? AND ip_address = ?
+        # ", start_time, end_time, ip_address)
+        #
+        # statement.execute(start_time, end_time, ip_address)
 
-        statement.execute(start_time, end_time, ip_address)
+        query = "
+  SELECT
+    ip_address,
+    AVG(if(rtt = 0, NULL, rtt)) AS avg_rtt,
+    MIN(if(rtt = 0, NULL, rtt)) AS min_rtt,
+    MAX(if(rtt = 0, NULL, rtt)) AS max_rtt,
+    quantile(0.5)(rtt) AS median_rtt,
+    sqrt(VAR_POP(rtt)) AS std_dev_rtt,
+    (countIf(rtt = 0) / count(*)) * 100 AS packet_loss_percent
+  FROM ip_stats
+  WHERE timestamp >= ? AND timestamp < ? AND ip_address = ?
+"
+
+        result = connection.query(query, [start_time, end_time, ip_address])
       end
 
       def batch_insert_ping_results(ping_results)
--- config/providers/database.rb
+++ config/providers/database.rb
@@ -3,11 +3,29 @@
 Hanami.app.register_provider :database, namespace: true do
   prepare do
     require 'rom'
+    require "rom/sql/clickhouse"
 
     config = ROM::Configuration.new(:sql, target['settings'].database_url)
 
     register 'config', config
     register 'connection', config.gateways[:default].connection
+
+
+    config = ROM::Configuration.new(
+      :sql,
+      "clickhouse://#{settings.clickhouse_username}:#{settings.clickhouse_password}@#{settings.clickhouse_host}:#{settings.clickhouse_port}/#{settings.clickhouse_database}"
+    ) do |conf|
+      conf.adapter :clickhouse
+      conf.use :macros
+    end
+
+    register "config", config
+
+    container = ROM.container(config) do |container|
+      container.relation(:ip_stats)
+    end
+
+    register "container", container
   end
 
   start do
